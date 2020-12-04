cad22359af4c9a2df0ca41c72bcee9059fc4de84
ialoig <giovannilai86@gmail.com>
2020-12-04 18:28:29 +0100

    Added backend implementation

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const express = require('express');
+const fileupload = require("express-fileupload");
+const app = express();
+const cors = require("cors");
+
+require('dotenv').config();
+
+//setting middleware
+app.use(cors());
+app.use(express.json()); //a middleware that extract req.body
+app.use(fileupload()); //a middleware that extract req.files
+
+//port connection
+const port = process.env.PORT || 5000;
+
+//Database connection
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => {
+        console.log("Connected to MongoDb")
+    })
+    .catch( (err) => {
+        console.log("Error connecting to MondoDb: " +err)
+    });
+const db = mongoose.connection;
+db.once('open', () => {
+    console.log("MongoDB database connection established successfully");    
+});
+
+
+//API endpoint
+const imagesRouter = require('./routes/api/image');
+app.use('/api/image', imagesRouter); 
+
+app.listen(port, () => {
+    console.log("Server is running on port: ", port);
+});
