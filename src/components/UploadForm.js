c1fe48fa0f64181b2e2a9333022a626570bd854b
ialoig <giovannilai86@gmail.com>
2020-12-07 19:37:27 +0100

    Added api request & image preview

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -1,50 +1,44 @@
 import React, { useState } from "react";
-import axios from "axios";
-import history from "../services/history";
 
 import icon from "../images/corner-left-up.svg"
+import ImageGrid from "./ImageGrid";
 
 const UploadForm = () => {
 
-    const [file, setFile] = useState(null);
+    const [file, setFile] = useState({
+        data: null,
+        preview: ""
+    });
     const [error, setError] = useState(null);
 
     const types = ['image/png', 'image/jpeg'];
 
     const onChange = (e) => {
         let selectedFile = e.target.files[0];
+        console.log("selectedFile:" +selectedFile)
         if (selectedFile && !types.includes(selectedFile.type)) {
             console.log("Not an image file: " +selectedFile.name)
             setError("Not an image file");
-            setFile(null)
+            setFile({
+                data: null,
+                preview: ""
+            });
         } else {
-            console.log("Yes is an image!" +selectedFile.name)
+            console.log("Yes is an image: " +selectedFile.name)
             setError("");
-            setFile(selectedFile);
-            uploadFile(file);
+            setFile({
+                data: selectedFile,
+                preview: URL.createObjectURL(selectedFile)
+            });
         }
-    } 
-
-    const uploadFile = async ( file ) => {
-        console.log("file name:" +file.name)
-        console.log("file type:" +file.type)
-        const newImage = {
-            name: file.name,
-            file: file
-        }
-
-        const res = await axios
-            .post("/new", newImage)
-            .then(() => history.push("/explore")) //redirect to explore
-            .catch(err => console.log("error while uploading new image: " +err)
-            );
     }
+
     return (
         <>
             <div className="">
                 { error && <div className="error">{ error }</div>}
-                { file && <div>{ file.name }</div> }
-                {!file && <label htmlFor="upload-image" className="flex items-center justify-center flex-col">
+                { file.data && <ImageGrid image={file}/> }
+                { !file.data && <label htmlFor="upload-image" className="flex items-center justify-center flex-col">
                     <img src={icon} alt="upload"></img>
                     <p className="font-semibold text-xl">Click here to upload your images!</p>
                     <input type="file" accept="image/*" id="upload-image" className="hidden" onChange={onChange} />
